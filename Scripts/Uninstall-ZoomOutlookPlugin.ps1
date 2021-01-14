ab3ae0484f60fa61539991106811b5dd14274e13
Tony Pagliaro <tony@pagliaro.co>
2021-01-14 13:52:03 -0500

    Add conditional before action.

--- Scripts/Uninstall-ZoomOutlookPlugin.ps1
+++ Scripts/Uninstall-ZoomOutlookPlugin.ps1
@@ -4,22 +4,30 @@ param($LogPath = 'C:\Windows\Temp\ZoomAddinUninstall.log')
 (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tonypags/PsWinAdmin/master/Get-InstalledSoftware.ps1')|iex;
 
 # Get the uninstall string from HKLM
-$RegInfo = Get-InstalledSoftware | Where-Object {$.Name -eq 'Zoom Outlook Plugin'}
+$AppSearchTerm = 'Zoom Outlook Plugin'
+$RegInfo = Get-InstalledSoftware | Where-Object {$.Name -eq $AppSearchTerm}
 $rawUninstallString = $RegInfo.UninstallCommand
 
-# Modify the string
-$newUninstallString = $rawUninstallString.Replace('/I','/X').Replace('{','"{').Replace('}','}"')
-[string]$UninstallString = $newUninstallString + ' /qn /norestart /log ' + $LogPath
+# Check to make sure we have something to do
+if (-not $rawUninstallString) {
+    Write-Warning "There is no $AppSearchTerm to remove. Exiting Script"
+} else {
 
-Try {
-    
-    Write-Verbose "Attempting to remove $($RegInfo.Name) v$($RegInfo.Version) with Install Date $($RegInfo.InstallDate)..."
-    $Result = Invoke-Expression $UninstallString -ea Stop -ev rem
-    Write-Output $Result
+    # Modify the string
+    $newUninstallString = $rawUninstallString.Replace('/I','/X').Replace('{','"{').Replace('}','}"')
+    [string]$UninstallString = $newUninstallString + ' /qn /norestart /log ' + $LogPath
 
-    Write-Verbose "A log is available with the MSI results."
-    Get-Item $LogPath -ea 0
+    Try {
 
-} Catch {
-    Write-Warning "Exception Raised: $($rem.Exception.Message)"
-}
+        Write-Verbose "Attempting to remove $($RegInfo.Name) v$($RegInfo.Version) with Install Date $($RegInfo.InstallDate)..."
+        $Result = Invoke-Expression $UninstallString -ea Stop -ev rem
+        Write-Output $Result
+
+        Write-Verbose "A log is available with the MSI results."
+        Get-Item $LogPath -ea 0
+
+    } Catch {
+        Write-Warning "Exception Raised: $($rem.Exception.Message)"
+    }
+
+}#END: if (-not $rawUninstallString)
